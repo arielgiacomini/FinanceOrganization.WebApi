b681e06a1cf7e9a13aed88762f6ab651ff177975
Ariel Giacomini <giacomini.ariel@gmail.com>
2024-01-07 16:24:45 -0300

    Criando o fomulário para criar as contas a pagar

---
+++ src/App.Forms/Forms/Initial.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace App.Forms.Forms
+{
+    partial class Initial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblContaPagarNameDescription = new Label();
+            txtContaPagarNameDescription = new TextBox();
+            lblContaPagarCategory = new Label();
+            cboContaPagarCategory = new ComboBox();
+            lblContaPagarTipoConta = new Label();
+            cboContaPagarTipoConta = new ComboBox();
+            lblContaPagarAnoMesInicial = new Label();
+            cboContaPagarAnoMesInicial = new ComboBox();
+            ckbContaPagarConsideraMesmoMes = new CheckBox();
+            cboContaPagarAnoMesFinal = new ComboBox();
+            lblContaPagarAnoMesFinal = new Label();
+            SuspendLayout();
+            // 
+            // lblContaPagarNameDescription
+            // 
+            lblContaPagarNameDescription.AutoSize = true;
+            lblContaPagarNameDescription.Location = new Point(19, 17);
+            lblContaPagarNameDescription.Name = "lblContaPagarNameDescription";
+            lblContaPagarNameDescription.Size = new Size(99, 15);
+            lblContaPagarNameDescription.TabIndex = 0;
+            lblContaPagarNameDescription.Text = "Nome/Descrição:";
+            // 
+            // txtContaPagarNameDescription
+            // 
+            txtContaPagarNameDescription.Location = new Point(124, 14);
+            txtContaPagarNameDescription.Name = "txtContaPagarNameDescription";
+            txtContaPagarNameDescription.Size = new Size(303, 23);
+            txtContaPagarNameDescription.TabIndex = 1;
+            // 
+            // lblContaPagarCategory
+            // 
+            lblContaPagarCategory.AutoSize = true;
+            lblContaPagarCategory.Location = new Point(19, 53);
+            lblContaPagarCategory.Name = "lblContaPagarCategory";
+            lblContaPagarCategory.Size = new Size(61, 15);
+            lblContaPagarCategory.TabIndex = 2;
+            lblContaPagarCategory.Text = "Categoria:";
+            // 
+            // cboContaPagarCategory
+            // 
+            cboContaPagarCategory.FormattingEnabled = true;
+            cboContaPagarCategory.Location = new Point(86, 50);
+            cboContaPagarCategory.Name = "cboContaPagarCategory";
+            cboContaPagarCategory.Size = new Size(188, 23);
+            cboContaPagarCategory.TabIndex = 3;
+            // 
+            // lblContaPagarTipoConta
+            // 
+            lblContaPagarTipoConta.AutoSize = true;
+            lblContaPagarTipoConta.Location = new Point(19, 85);
+            lblContaPagarTipoConta.Name = "lblContaPagarTipoConta";
+            lblContaPagarTipoConta.Size = new Size(81, 15);
+            lblContaPagarTipoConta.TabIndex = 4;
+            lblContaPagarTipoConta.Text = "Tipo de Conta";
+            // 
+            // cboContaPagarTipoConta
+            // 
+            cboContaPagarTipoConta.FormattingEnabled = true;
+            cboContaPagarTipoConta.Location = new Point(117, 82);
+            cboContaPagarTipoConta.Name = "cboContaPagarTipoConta";
+            cboContaPagarTipoConta.Size = new Size(167, 23);
+            cboContaPagarTipoConta.TabIndex = 5;
+            // 
+            // lblContaPagarAnoMesInicial
+            // 
+            lblContaPagarAnoMesInicial.AutoSize = true;
+            lblContaPagarAnoMesInicial.Location = new Point(21, 124);
+            lblContaPagarAnoMesInicial.Name = "lblContaPagarAnoMesInicial";
+            lblContaPagarAnoMesInicial.Size = new Size(93, 15);
+            lblContaPagarAnoMesInicial.TabIndex = 7;
+            lblContaPagarAnoMesInicial.Text = "Ano/Mês Inicial:";
+            // 
+            // cboContaPagarAnoMesInicial
+            // 
+            cboContaPagarAnoMesInicial.FormattingEnabled = true;
+            cboContaPagarAnoMesInicial.Location = new Point(118, 121);
+            cboContaPagarAnoMesInicial.Name = "cboContaPagarAnoMesInicial";
+            cboContaPagarAnoMesInicial.Size = new Size(161, 23);
+            cboContaPagarAnoMesInicial.TabIndex = 8;
+            // 
+            // ckbContaPagarConsideraMesmoMes
+            // 
+            ckbContaPagarConsideraMesmoMes.AutoSize = true;
+            ckbContaPagarConsideraMesmoMes.Checked = true;
+            ckbContaPagarConsideraMesmoMes.CheckState = CheckState.Checked;
+            ckbContaPagarConsideraMesmoMes.Location = new Point(285, 123);
+            ckbContaPagarConsideraMesmoMes.Name = "ckbContaPagarConsideraMesmoMes";
+            ckbContaPagarConsideraMesmoMes.Size = new Size(214, 19);
+            ckbContaPagarConsideraMesmoMes.TabIndex = 9;
+            ckbContaPagarConsideraMesmoMes.Text = "Considera como Mês Inicial e Final?";
+            ckbContaPagarConsideraMesmoMes.UseVisualStyleBackColor = true;
+            ckbContaPagarConsideraMesmoMes.CheckedChanged += ckbContaPagarConsideraMesmoMes_CheckedChanged;
+            // 
+            // cboContaPagarAnoMesFinal
+            // 
+            cboContaPagarAnoMesFinal.FormattingEnabled = true;
+            cboContaPagarAnoMesFinal.Location = new Point(118, 150);
+            cboContaPagarAnoMesFinal.Name = "cboContaPagarAnoMesFinal";
+            cboContaPagarAnoMesFinal.Size = new Size(161, 23);
+            cboContaPagarAnoMesFinal.TabIndex = 11;
+            // 
+            // lblContaPagarAnoMesFinal
+            // 
+            lblContaPagarAnoMesFinal.AutoSize = true;
+            lblContaPagarAnoMesFinal.Location = new Point(21, 153);
+            lblContaPagarAnoMesFinal.Name = "lblContaPagarAnoMesFinal";
+            lblContaPagarAnoMesFinal.Size = new Size(87, 15);
+            lblContaPagarAnoMesFinal.TabIndex = 10;
+            lblContaPagarAnoMesFinal.Text = "Ano/Mês Final:";
+            // 
+            // Initial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(507, 275);
+            Controls.Add(cboContaPagarAnoMesFinal);
+            Controls.Add(lblContaPagarAnoMesFinal);
+            Controls.Add(ckbContaPagarConsideraMesmoMes);
+            Controls.Add(cboContaPagarAnoMesInicial);
+            Controls.Add(lblContaPagarAnoMesInicial);
+            Controls.Add(cboContaPagarTipoConta);
+            Controls.Add(lblContaPagarTipoConta);
+            Controls.Add(cboContaPagarCategory);
+            Controls.Add(lblContaPagarCategory);
+            Controls.Add(txtContaPagarNameDescription);
+            Controls.Add(lblContaPagarNameDescription);
+            Name = "Initial";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Tela inicial - Organização Financeira";
+            Load += Initial_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblContaPagarNameDescription;
+        private TextBox txtContaPagarNameDescription;
+        private Label lblContaPagarCategory;
+        private ComboBox cboContaPagarCategory;
+        private Label lblContaPagarTipoConta;
+        private ComboBox cboContaPagarTipoConta;
+        private Label lblContaPagarAnoMesInicial;
+        private ComboBox cboContaPagarAnoMesInicial;
+        private CheckBox ckbContaPagarConsideraMesmoMes;
+        private ComboBox cboContaPagarAnoMesFinal;
+        private Label lblContaPagarAnoMesFinal;
+    }
+}
---
+++ src/App.Forms/Forms/Initial.cs
@@ -0,0 +1,125 @@
+﻿using Domain.Utils;
+
+namespace App.Forms.Forms
+{
+    public partial class Initial : Form
+    {
+        public Initial()
+        {
+            InitializeComponent();
+        }
+
+        private void Initial_Load(object sender, EventArgs e)
+        {
+            PreencherComboBoxContaPagarCategory();
+            PreencherComboBoxContaPagarTipoConta();
+            PreencherComboBoxAnoMes();
+            RegraCamposAnoMes();
+        }
+
+        private void PreencherComboBoxContaPagarCategory()
+        {
+            Dictionary<int, string> categoriasContaPagar = new()
+            {
+                { 1, "Alimentação:Almoço" },
+                { 2, "Alimentação:Besteiras" },
+                { 3, "Alimentação:Café da Manhã" },
+                { 4, "Alimentação:Feira" },
+                { 5, "Alimentação:Jantar" },
+                { 6, "Alimentação:Mercado" },
+                { 7, "Automóvel:Combustível" },
+                { 8, "Automóvel:Estacionamento" },
+                { 9, "Automóvel:Garagem" },
+                { 10, "Automóvel:Seguro" },
+                { 11, "Casa:Essencial" },
+                { 12, "Casa:Financiamento" },
+                { 13, "Casa:Internet" },
+                { 14, "Casa:Segurança" },
+                { 15, "Dizimo" },
+                { 16, "Esporte:Helena" },
+                { 17, "Esporte:TaxaMatricula" },
+                { 18, "Filhos:Helena" },
+                { 19, "Filhos:Leo" },
+                { 20, "Gasto de Terceiros" },
+                { 21, "Serviço:Cloud" },
+                { 22, "Serviço:Produtividade" },
+                { 23, "Serviço:Streaming" },
+                { 24, "Transporte:Escolar" }
+            };
+
+            foreach (var item in categoriasContaPagar)
+            {
+                cboContaPagarCategory.Items.Add(item.Value);
+            }
+
+            var positionOne = categoriasContaPagar.FirstOrDefault();
+
+            cboContaPagarCategory.SelectedItem = positionOne.Value;
+        }
+
+        private void PreencherComboBoxContaPagarTipoConta()
+        {
+            Dictionary<int, string> tipoConta = new()
+            {
+                { 1, "Cartão de Crédito" },
+                { 2, "Cartão de Débito" },
+                { 3, "Cartão VA" },
+                { 4, "Cartão VR" },
+                { 5, "Itaú" }
+            };
+
+            foreach (var item in tipoConta)
+            {
+                cboContaPagarTipoConta.Items.Add(item.Value);
+            }
+
+            var positionOne = tipoConta.FirstOrDefault();
+
+            cboContaPagarTipoConta.SelectedItem = positionOne.Value;
+        }
+
+        private void PreencherComboBoxAnoMes()
+        {
+            var dateTimeCorteInitial = DateTime.Now.AddMonths(-3);
+
+            DateTime firstDate = new(dateTimeCorteInitial.Year, dateTimeCorteInitial.Month, 1, 0, 0, 0, DateTimeKind.Local);
+
+            Dictionary<int, string> yearMonths = new();
+
+            for (int i = 0; i < 27; i++)
+            {
+                var dateAdd = firstDate.AddMonths(i);
+
+                var yearMonthAdd = DateServiceUtils.GetYearMonthPortugueseByDateTime(dateAdd);
+
+                yearMonths.Add(i, yearMonthAdd);
+
+                cboContaPagarAnoMesInicial.Items.Add(yearMonthAdd);
+                cboContaPagarAnoMesFinal.Items.Add(yearMonthAdd);
+            }
+
+            _ = yearMonths.TryGetValue(3, out string currentYearMont);
+
+            cboContaPagarAnoMesInicial.SelectedItem = currentYearMont;
+            cboContaPagarAnoMesFinal.SelectedItem = currentYearMont;
+        }
+
+        private void ckbContaPagarConsideraMesmoMes_CheckedChanged(object sender, EventArgs e)
+        {
+            RegraCamposAnoMes();
+        }
+
+        private void RegraCamposAnoMes()
+        {
+            if (ckbContaPagarConsideraMesmoMes.Checked)
+            {
+                cboContaPagarAnoMesFinal.SelectedItem = cboContaPagarAnoMesInicial.SelectedItem;
+                cboContaPagarAnoMesFinal.Enabled = false;
+            }
+            else
+            {
+                cboContaPagarAnoMesFinal.Enabled = true;
+            }
+        }
+    }
+}
