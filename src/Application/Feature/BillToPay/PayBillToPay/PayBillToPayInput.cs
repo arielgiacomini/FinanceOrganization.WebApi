d117fc6abb238b1374e9f40d99ae326ff66cdcc8
Ariel Giacomini <arieltecnologia@outlook.com>
2025-06-28 07:24:47 -0300

    Adicionando nova propriedade que faz pagamento antecipado

--- src/Application/Feature/BillToPay/PayBillToPay/PayBillToPayInput.cs
+++ src/Application/Feature/BillToPay/PayBillToPay/PayBillToPayInput.cs
@@ -8,6 +8,14 @@
         public DateTime LastChangeDate { get; set; }
         public string? YearMonth { get; set; }
         public string? Account { get; set; }
+        /// <summary>
+        /// Ideal para adiantamento de pagamento de cartão de crédito por exemplo, a fatura fecha no dia X porém descido pagar antecipado.
+        /// </summary>
+        public bool? AdvancePayment { get; set; }
+        /// <summary>
+        /// Indicates if the payment should be considered for Naira credit card transactions.
+        /// </summary>
+        [Obsolete]
         public bool? ConsiderNairaCreditCard { get; set; }
     }
 }
