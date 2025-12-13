957330958bccc624ebe206914a80ed2caebce95e
Ariel Giacomini <arieltecnologia@outlook.com>
2025-12-13 19:53:28 -0300

    Ajuste para categorias

--- src/Domain/Interfaces/ICategoryRepository.cs
+++ src/Domain/Interfaces/ICategoryRepository.cs
@@ -6,7 +6,7 @@ namespace Domain.Interfaces
     public interface ICategoryRepository
     {
         Task<IList<Category>?> GetAllAsync(AccountType accountType);
-        Task<IList<string>?> GetNonRegister();
+        Task<IList<Category>> GetNonRegister();
         Task<int> SaveRange(IList<Category> categories);
     }
 }
