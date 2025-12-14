ee59f8a4e9cb22c1fb0e5202c62db0dc2f2f0cd5
Ariel Giacomini <arieltecnologia@outlook.com>
2025-12-14 09:10:13 -0300

    Ajuste de cadastro automático de categoria

--- src/Application/EventHandlers/CreateCategoryEvent/CreateCategoryEventHandler.cs
+++ src/Application/EventHandlers/CreateCategoryEvent/CreateCategoryEventHandler.cs
@@ -23,14 +23,49 @@ namespace Application.EventHandlers.CreateCategoryEvent
 
         public async Task Handle(CreateCategoryEventInput input)
         {
-            var categoriesNonRegister = await _categoryRepository.GetNonRegister();
+            var categoriesToActions = await _categoryRepository.GetCategoriesToActions();
 
-            if (categoriesNonRegister == null)
+            if (categoriesToActions == null || categoriesToActions.IsEmpty)
             {
+                _logger.Error("Os dicionarios de categorias para ações não trouxe nenhum registro válido.");
                 return;
             }
 
-            var result = await _categoryRepository.SaveRange(categoriesNonRegister);
+            #region Passo 1 - Adiciona as categorias que estão na tabela de contas a pagar e não existem na tabela de categorias
+
+            if (categoriesToActions["Adicionar"].Count > 0)
+            {
+                _logger.Information("Adiciona {Count} categorias novas. Categorias: {@Categories} ", categoriesToActions["Adicionar"].Count, categoriesToActions["Adicionar"]);
+
+                await _categoryRepository.SaveRange(categoriesToActions["Adicionar"]);
+            }
+
+            #endregion Passo 1 - Adiciona as categorias que estão na tabela de contas a pagar e não existem na tabela de categorias
+
+            #region Passo 2 - Habilita as categorias que voltaram a existir na tabela de contas a pagar e que estavam inativadas.
+
+            if (categoriesToActions["Habilitar"].Count > 0)
+            {
+                // Log the categories that will be enabled (fix: use categoriesToEnable)
+                _logger.Information("Habilita as categorias que voltaram a existir na tabela de contas a pagar: {@Categories}", categoriesToActions["Habilitar"]);
+
+                await _categoryRepository.SetEnableOrDisableCategoryByRange(categoriesToActions["Habilitar"], true);
+            }
+
+            #endregion Passo 2 - Habilita as categorias que voltaram a existir na tabela de contas a pagar
+
+            #region Passo 3 - Desabilita as categorias que não existem mais na tabela de contas a pagar.
+
+            if (categoriesToActions["Desabilitar"].Count > 0)
+            {
+                _logger.Information("Desabilita as categorias que não existem mais na tabela de Contas a Pagar: {@categories}", categoriesToActions["Desabilitar"]);
+
+                await _categoryRepository.SetEnableOrDisableCategoryByRange(categoriesToActions["Desabilitar"], false);
+            }
+
+            #endregion Passo 3 - Desabilita as categorias que não existem mais na tabela de contas a pagar
+
+            _categoryRepository.ClearTempCategories();
         }
     }
 }
--- src/Domain/Interfaces/ICategoryRepository.cs
+++ src/Domain/Interfaces/ICategoryRepository.cs
@@ -1,12 +1,15 @@
 ﻿using Domain.Entities;
 using Domain.Entities.Enums;
+using System.Collections.Concurrent;
 
 namespace Domain.Interfaces
 {
     public interface ICategoryRepository
     {
-        Task<IList<Category>?> GetAllAsync(AccountType accountType);
-        Task<IList<Category>> GetNonRegister();
+        void ClearTempCategories();
+        Task<IList<Category>?> GetAllAsync(AccountType accountType, bool? filterEnable = null);
+        Task<ConcurrentDictionary<string, IList<Category>>> GetCategoriesToActions();
         Task<int> SaveRange(IList<Category> categories);
+        Task<bool> SetEnableOrDisableCategoryByRange(IList<Category> categoriesNotExists, bool enable);
     }
 }
